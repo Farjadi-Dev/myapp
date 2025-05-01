158f6afd8f81818f8f643cd2e1d8feebc2d00c71
hoseinfarjady <hoseinfarjady@gmail.com>
2025-05-01 13:33:25 +0000

    dark them

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/chapter_list_screen.dart';
 
 void main() {
@@ -7,13 +8,33 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Question App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChapterListScreen(),
+  Widget build(BuildContext context) {    
+    final darkColorScheme = ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.dark,
     );
+    final darkTheme = ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+        textTheme: GoogleFonts.montserratTextTheme().apply(
+          bodyColor: darkColorScheme.onBackground,
+          displayColor: darkColorScheme.onBackground,
+        ),
+        appBarTheme: AppBarTheme(
+            backgroundColor: darkColorScheme.surfaceVariant,
+            foregroundColor: darkColorScheme.onSurfaceVariant,
+            titleTextStyle: TextStyle(
+                color: darkColorScheme.onSurfaceVariant,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)));
+
+    return MaterialApp(      
+        title: 'Question App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.dark,
+        home: ChapterListScreen());
   }
 }
--- lib/screens/chapter_list_screen.dart
+++ lib/screens/chapter_list_screen.dart
@@ -24,35 +24,40 @@ class _ChapterListScreenState extends State<ChapterListScreen> {
               ],
             ),
           ),
-        ),
+        ),        
         title: const Text(
           'Select Chapter',
-          style: TextStyle(
-            ),
-          ),
         ),
       ),
       body: ListView.builder(
-        itemCount: chapters.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Card(              
-                elevation: 2.0,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                child: ListTile(title: Text(chapters[index]), onTap: () {
-                  Navigator.push(context, PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) =>
-                        QuestionListScreen(chapterTitle: chapters[index]),
-                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                      return FadeTransition(opacity: animation, child: child);
-                    },
-                  ));
-                })
-            ),
-          );
-        },
-      ),
-    );
+          itemCount: chapters.length,
+          itemBuilder: (context, index) {
+            print('Building item at index: $index');
+            return Padding( // Padding Widget start
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // Padding
+              child: Card( // Card Widget start
+                elevation: 2.0, // Card elevation
+                shape: RoundedRectangleBorder( // Card Shape
+                  borderRadius: BorderRadius.circular(10.0), // Card border radius
+                ),
+                child: ListTile( // ListTile start
+                  title: Text(chapters[index]), // ListTile title
+                  onTap: () { // ListTile onTap
+                    Navigator.push( // Navigator push
+                      context, // context
+                      PageRouteBuilder( // PageRouteBuilder
+                        pageBuilder: (context, animation, secondaryAnimation) => QuestionListScreen(chapterTitle: chapters[index]),
+                        transitionsBuilder: (context, animation, secondaryAnimation, child) => FadeTransition(opacity: animation, child: child),
+                      ), // PageRouteBuilder
+                    ); // Navigator push
+                  },
+                ), // ListTile end
+              ), // Card Widget end
+            ); // Padding Widget end
+          },
+        ),
+      );
+
   }
 }
+
